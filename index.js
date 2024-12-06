f79060251b5d935607ec5bc663b4db70d8557737
developerakashy <developerakashy@gmail.com>
2024-12-06 15:09:17 +0530

    shifted to date object from manual counting

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-const hour = document.querySelector(".hour");
-const minute = document.querySelector(".minute");
-const second = document.querySelector(".second");
-const millisecond = document.querySelector(".ms");
+const hourPara = document.querySelector(".hour");
+const minutePara = document.querySelector(".minute");
+const secondPara = document.querySelector(".second");
+const millisecondPara = document.querySelector(".ms");
 
-const minuteColonSecond = document.querySelector(".minute-colon-second")
 const hourColonMinute = document.querySelector(".hour-colon-minute")
 
 const playBtn = document.querySelector(".play-btn");
@@ -12,20 +11,27 @@ const resetBtn = document.querySelector(".reset");
 const timelapsBtn = document.querySelector(".time-laps");
 
 const lapsHourColonMinute = document.querySelector(".laps-hour-colon-minute");
-const lapsMinuteColonSecond = document.querySelector(".laps-minute-colon-second");
 
 const lapsHour = document.querySelector(".laps-hour");
 const lapsMinute = document.querySelector(".laps-minute");
 const lapsSecond = document.querySelector(".laps-second");
 const lapsMillisecond = document.querySelector(".laps-millisecond");
 
-const header = document.querySelector('header')
 const lapsList = document.querySelector('.laps-list')
 const lapsDisplay = document.querySelector('.laps-display')
 const lapsTime = document.querySelector('.laps-time')
 
-let highestLapTime = '2024-10-10T00:00:00'
-let lowestLapTime = '2024-10-10T23:59:59'
+let highestLapTime = 0
+let lowestLapTime = 100000
+
+let timeCaptured = 0
+let lapTimeCaptured = 0
+
+let totalMillisecond = 0
+let millisecond = 0
+
+let totalLapMillisecond = 0
+let lapMillisecond = 0
 
 let timeInterval
 let lapTimingsArray = []
@@ -40,105 +46,75 @@ let lapSecondCount = 0
 let lapMinuteCount = 0
 let lapHourCount = 0
 
-function updateCurrentTiming(){
-  millisecondCount += 1
-  lapMillisecondCount += 1
-
-    if(millisecondCount === 10){
-      secondCount += 1
-      millisecondCount = 0
-
-      if(secondCount === 60){
-        minuteCount += 1
-        secondCount = 0
-
-        if(minuteCount === 60){
-          hourCount += 1
-          minuteCount = 0
-
-        }
-      }
-    }
-
-    if(lapMillisecondCount === 10){
-      lapSecondCount += 1
-      lapMillisecondCount = 0
-
-      if(lapSecondCount === 60){
-        lapMinuteCount += 1
-        lapSecondCount = 0
-
-        if(lapMinuteCount === 60){
-          lapHourCount += 1
-          lapMinuteCount = 0
-
-        }
-      }
-    }
-
-
-    showUpdatedTime()
-
-}
-
-
-function showUpdatedTime(){
-  millisecond.textContent = millisecondCount
-  lapsMillisecond.textContent = lapMillisecondCount
-
-  second.textContent = secondCount < 10 ? `0${secondCount}` : secondCount
-  lapsSecond.textContent = lapSecondCount < 10 ? `0${lapSecondCount}` : lapSecondCount
-
-  minute.textContent = minuteCount < 10 ? `0${minuteCount}` : minuteCount
-  lapsMinute.textContent = lapMinuteCount < 10 ? `0${lapMinuteCount}` : lapMinuteCount
-
-  hour.textContent = hourCount < 10 ? `0${hourCount}` : hourCount
-  lapsHour.textContent = lapHourCount < 10 ? `0${lapHourCount}` : lapHourCount
-
-
-  if(minuteCount > 0 || hourCount > 0){
-    minute.classList.remove('hidden')
-    minuteColonSecond.classList.remove('hidden')
-
-    if(hourCount > 0){
-        hour.classList.remove('hidden')
-        hourColonMinute.classList.remove('hidden')
-    }
-  }
-
-  if(lapMinuteCount > 0 || lapHourCount > 0){
-    lapsMinute.classList.remove('hidden')
-    lapsMinuteColonSecond.classList.remove('hidden')
-
-    if(lapHourCount > 0){
-      lapsHour.classList.remove('hidden')
-      lapsHourColonMinute.classList.remove('hidden')
-    }
-  }
-
-
-}
-
-
 playBtn.addEventListener('click', (e) => startOrResumeTheStopWatch(e))
 pauseBtn.addEventListener('click', (e) => pauseTheStopWatch(e))
 
 timelapsBtn.addEventListener('click', showAndResetTimeLaps)
 resetBtn.addEventListener('click', resetTheStopWatch)
 
+function updateCurrentTiming(){
+
+  millisecond = Math.floor((Date.now() - timeCaptured) / 10) + totalMillisecond
+  lapMillisecond = Math.floor((Date.now() - lapTimeCaptured) / 10) + totalLapMillisecond
+
+  let second = Math.floor(millisecond / 100)
+  let minute = Math.floor(second / 60)
+  let hour = Math.floor(minute / 60)
+
+  let lapSecond = Math.floor(lapMillisecond / 100)
+  let lapMinute = Math.floor(lapSecond / 60)
+  let lapHour = Math.floor(lapMinute / 60)
+
+  millisecondCount = ((millisecond % 100) / 10)
+  secondCount = second % 60 < 10 ? `0${second % 60}` : second % 60
+  minuteCount = minute % 60 < 10 ? `0${minute % 60}` : minute % 60
+  hourCount = hour < 10 ? `0${hour}` : hour
+
+  lapMillisecondCount = ((lapMillisecond % 100) / 10)
+  lapSecondCount = lapSecond % 60 < 10 ? `0${lapSecond % 60}` : lapSecond % 60
+  lapMinuteCount = lapMinute % 60 < 10 ? `0${lapMinute % 60}` : lapMinute % 60
+  lapHourCount = lapHour < 10 ? `0${lapHour}` : lapHour
+
+  showUpdatedTime()
+
+}
 
 
+function showUpdatedTime(){
+  millisecondPara.textContent = millisecondCount
+  secondPara.textContent = secondCount
+  minutePara.textContent = minuteCount
+  hourPara.textContent = hourCount
+
+  lapsMillisecond.textContent = lapMillisecondCount
+  lapsSecond.textContent = lapSecondCount
+  lapsMinute.textContent = lapMinuteCount
+  lapsHour.textContent = lapHourCount
+
+  if(Number(hourCount) > 0){
+      hourPara.classList.remove('hidden')
+      hourColonMinute.classList.remove('hidden')
+  }
+
+  if(Number(lapHourCount) > 0){
+    lapsHour.classList.remove('hidden')
+    lapsHourColonMinute.classList.remove('hidden')
+  }
+
+}
 
 
 function startOrResumeTheStopWatch(e){
+    timeCaptured = Date.now()
+    lapTimeCaptured = Date.now()
     timeInterval = setInterval(updateCurrentTiming, 100)
 
     let element = e.currentTarget
 
     playBtn.classList.add('hidden')
     pauseBtn.classList.remove('hidden')
-    resetBtn.classList.remove('hidden')
-    timelapsBtn.classList.remove('hidden')
+    resetBtn.classList.remove('visibility-hidden')
+    timelapsBtn.classList.remove('visibility-hidden')
 
     setTimeout(function(){
       resizeBtn(element)
@@ -147,13 +123,15 @@ function startOrResumeTheStopWatch(e){
 }
 
 function pauseTheStopWatch(e){
+    totalMillisecond = millisecond
+    totalLapMillisecond = lapMillisecond
     clearInterval(timeInterval)
 
     let element = e.currentTarget
 
     playBtn.classList.remove('hidden')
     pauseBtn.classList.add('hidden')
-    timelapsBtn.classList.add('hidden')
+    timelapsBtn.classList.add('visibility-hidden')
 
     setTimeout(function(){
       resizeBtn(element)
@@ -166,58 +144,36 @@ function resizeBtn(btn){
 
     playBtn.style.width = btn ? '100px' : '170px';
     pauseBtn.style.width = btn ? '100px' : '170px';
-    playBtn.style.borderRadius = btn ? '50%' : '100px';
-    pauseBtn.style.borderRadius = btn ? '50%' : '100px';
+    playBtn.style.borderRadius = btn ? '50%' : '40px';
+    pauseBtn.style.borderRadius = btn ? '50%' : '40px';
 }
 
 function showAndResetTimeLaps(){
+
   lapsTime.style.display = 'flex'
 
   let isHighestLap = false
   let isLowestLap = false
 
-  let lapHour = lapHourCount < 10 ? `0${lapHourCount}` : lapHourCount
-  let lapMinute = lapMinuteCount < 10 ? `0${lapMinuteCount}` : lapMinuteCount
-  let lapSecond = lapSecondCount < 10 ? `0${lapSecondCount}` : lapSecondCount
-  let lapMillisecond = lapMillisecondCount
-
-  let overallHour = hourCount < 10 ? `0${hourCount}` : hourCount
-  let overallMinute = minuteCount < 10 ? `0${minuteCount}` : minuteCount
-  let overallSecond = secondCount < 10 ? `0${secondCount}` : secondCount
-  let overallMillisecond = millisecondCount
-
-  let timeString = `2024-10-10T${lapHour}:${lapMinute}:${lapSecond}.${lapMillisecond}`
-
-  let currentLapTime = Date.parse(timeString)
-  let previousHighestLapTime = Date.parse(highestLapTime)
-  let previousLowestLapTime = Date.parse(lowestLapTime)
-
-
-  if(currentLapTime > previousHighestLapTime){
-    highestLapTime = timeString
-    changeIsHighestToFalse()
-  }
-  if(currentLapTime < previousLowestLapTime){
-    lowestLapTime = timeString
-    changeIsLowestToFalse()
-  }
-
-
-  function changeIsHighestToFalse(){
+  if(lapMillisecond > highestLapTime){
+    highestLapTime = lapMillisecond
     lapTimingsArray = lapTimingsArray.map(lap => lap.isHighest ? {...lap, isHighest: false} : lap)
     isHighestLap = true
   }
 
-  function changeIsLowestToFalse(){
+  if(lapMillisecond < lowestLapTime){
+    lowestLapTime = lapMillisecond
     lapTimingsArray = lapTimingsArray.map(lap => lap.isLowest ? {...lap, isLowest: false} : lap)
     isLowestLap = true
   }
-  console.log(lapMinuteCount)
-  let lapTimeStr = lapHourCount ? `${lapHour}:${lapMinute}:${lapSecond}.${lapMillisecond}` :
-                   lapMinuteCount ? `${lapMinute}:${lapSecond}.${lapMillisecond}` : `${lapSecond}.${lapMillisecond}`
 
-  let overallTimeStr = hourCount ? `${overallHour}:${overallMinute}:${overallSecond}.${overallMillisecond}` :
-                       minuteCount ? `${overallMinute}:${overallSecond}.${overallMillisecond}` : `${overallSecond}.${overallMillisecond}`
+  let lapTimeStr = Number(lapHourCount) ?
+                  `${lapHourCount}:${lapMinuteCount}:${lapSecondCount}.${lapMillisecondCount}` :
+                  `${lapMinuteCount}:${lapSecondCount}.${lapMillisecondCount}`
+
+  let overallTimeStr = Number(hourCount) ?
+                      `${hourCount}:${minuteCount}:${secondCount}.${millisecondCount}` :
+                      `${minuteCount}:${secondCount}.${millisecondCount}`
 
   const lapInfo = {
     lapTime: lapTimeStr,
@@ -225,7 +181,6 @@ function showAndResetTimeLaps(){
     isHighest: isHighestLap,
     isLowest: isLowestLap
   }
-
 
   lapTimingsArray.push(lapInfo)
   resetTimeLaps()
@@ -236,15 +191,11 @@ function showAndResetTimeLaps(){
 function showTimeLaps(){
   lapsList.innerHTML = ``
 
-  if(!header.classList.contains('hidden')){
-    header.classList.add('hidden')
 
-    setTimeout(function(){
-      document.querySelector('.running-time').style.marginTop = '20px'
-      lapsDisplay.style.display = 'flex'
-    }, 0)
-
-  }
+  setTimeout(function(){
+    document.querySelector('.running-time').style.marginTop = '20px'
+    lapsDisplay.style.display = 'flex'
+  }, 0)
 
 
   lapTimingsArray.forEach((lap, index) => {
@@ -274,27 +225,38 @@ function resetTimeLaps(){
   lapMinuteCount = 0
   lapHourCount = 0
 
+  totalLapMillisecond = 0
+  totalMillisecond = millisecond
+  timeCaptured = Date.now()
+  lapTimeCaptured = Date.now()
+  clearInterval(timeInterval)
+  timeInterval = setInterval(updateCurrentTiming, 100)
+
+
   lapsHour.classList.add('hidden')
   lapsHourColonMinute.classList.add('hidden')
-  lapsMinute.classList.add('hidden')
-  lapsMinuteColonSecond.classList.add('hidden')
 }
 
 
 function resetTheStopWatch(){
+  resetTimeLaps()
   clearInterval(timeInterval)
 
-  header.classList.remove('hidden')
   lapsDisplay.style.display = 'none'
   lapsTime.style.display = 'none'
 
   millisecondCount = 0
-  secondCount = 0
-  minuteCount = 0
+  secondCount = `00`
+  minuteCount = `00`
   hourCount = 0
 
-  minute.classList.add('hidden')
-  hour.classList.add('hidden')
+  totalMillisecond = 0
+  totalLapMillisecond = 0
+  lapTimeCaptured = 0
+  timeCaptured = 0
+
+  hourPara.classList.add('hidden')
+  hourColonMinute.classList.add('hidden')
 
   setTimeout(function(){
     document.querySelector('.running-time').style.marginTop = '100px'
@@ -305,8 +267,8 @@ function resetTheStopWatch(){
     pauseBtn.style.borderRadius = '50%'
   }, 0)
 
-  resetBtn.classList.add('hidden')
-  timelapsBtn.classList.add('hidden')
+  resetBtn.classList.add('visibility-hidden')
+  timelapsBtn.classList.add('visibility-hidden')
 
   pauseBtn.classList.add('hidden')
   playBtn.classList.remove('hidden')
@@ -314,10 +276,9 @@ function resetTheStopWatch(){
   lapsList.innerHTML = ''
   lapTimingsArray = []
 
-  highestLapTime = '2024-10-10T00:00:00'
-  lowestLapTime = '2024-10-10T23:59:59'
+  highestLapTime = 0
+  lowestLapTime = 100000
 
-  resetTimeLaps()
   showUpdatedTime()
 
 }
